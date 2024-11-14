a048107c16c0aebf1085dc1334ba5bfe3b51c63b
Vladimir_O_Cutucador <146009567+ozamabinballen@users.noreply.github.com>
2024-11-14 17:15:55 -0300

    Add files via upload

---
+++ murilo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+'Geografia',
+'qual a capital da frança?',
+'a capital da frança é paris'
+)
+
+criaCartao(
+'programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+'lingua inglesa',
+'como se diz oi em ingles?',
+'Oi em ingles é HI (RAI)'
+)
